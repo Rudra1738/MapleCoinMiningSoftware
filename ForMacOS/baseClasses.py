f7a6c5d597d0a1c8be3bd73551f377db3fff84ff
Rudra1738 <65863182+Rudra1738@users.noreply.github.com>
2021-04-29 12:57:20 +0300

    Update baseClasses.py

--- ForMacOS/baseClasses.py
+++ ForMacOS/baseClasses.py
@@ -15,20 +15,36 @@ class Block():
         )
         self.miner = None
     
-    @staticmethod
-    def getMerkelRoot(items):
+    def getMerkelRoot(self, items):
+        """
+        Get the merkel root of a list of items
+        """
+
+        #Hash all the items in the given item list
+        items = [hashlib.sha3_512(str(item).encode('utf8')).hexdigest() for item in items]
+
+        #Base condition, return the final merkel root
         if len(items) == 1:
             return items[0]
+        elif len(items) == 0:
+            return ""
 
-        items = [hashlib.sha3_512(str(item).encode('utf8')).hexdigest() for item in items]
+        #add two hashes together and add the combined to the new items list
         newItems, temp = [], ""
         for index in range(len(items)):
+
+            #If divisible by 2, save it for next iteration
             if index % 2 == 0:
                 temp = items[index]
+                if index == len(items) - 1:
+                    newItems.append(temp)
             else:
+            #Otherwise add it to the saved item from previous iteration
                 newItems.append(temp + items[index])
                 temp = ""
-        return Block.getMerkelRoot(newItems)
+
+        #repeat the process
+        return self.getMerkelRoot(newItems)
     
     def __eq__(self, other):
         return isinstance(other, type(self)) and self.newBlockIndex == other.newBlockIndex and self.transactions == other.transactions and all([self.transactions[index] == other.transactions[index] for index in range(len(self.transactions))]) and self.nonce == other.nonce and self.hash == other.hash and self.merkelRoot == other.merkelRoot
